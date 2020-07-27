23d24fed645b5d88a85917f981c58ffaa15db558
Witni <witni.whittle@icloud.com>
2020-07-27 16:26:19 -0500

    Second commit

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 getWeather(72701);
-
 function getWeather(zipcode) {
     let request = new XMLHttpRequest();
+   
 
     request.onreadystatechange = function() {
         if (request.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
             if (request.status == 200) {
                 let forecast = JSON.parse(request.responseText);
                 console.log(forecast);
-                let conditions = forecast.name + " is currently " + forecast.weather[0].main + ", and " + forecast.main.temp + " degrees.";
-                console.log(conditions);
+                let conditions = `${forecast.name} is currently ${forecast.weather[0].main}, and ${forecast.main.temp} degrees.`;
+                {
                 document.getElementById("weather-message").innerText = conditions;
             }
-            else {
+              {
                 alert('There was an error');
             }
         }
     };
 
-    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "ccef2c8c8034982955bec1e0b2a83e36", true);
+    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",US&units=imperial&appid=ccef2c8c80349829", true);
     request.send();
+    }
 }
